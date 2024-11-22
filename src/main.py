65a467914b6da82bb6e9c959a4e8f159b87a037d
mathisaulagnier <mathis.aulagnier@telecomnancy.eu>
2024-11-21 21:25:08 -0500

    oui oui

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from data_preprocessing import load_data, clean_data, create_preprocessor, prepa
 from model_training import train_models
 from prediction import load_model, load_data_for_pred, predict, create_instance
 
+import pandas as pd
+
 from sklearn.pipeline import Pipeline
 
 import joblib
@@ -56,8 +58,98 @@ def test():
     predictions = predict(model_path, df, save_submition=True)
     print("Prédictions effectuées avec succès!")
 
+def estimate_its_house(lst):
+    # Créer une instance
+    model_path = '../Models/voting_regressor_model.pkl'
+    instance = create_instance(lst)
+    print("Instance créée avec succès!")
+    predictions = predict(model_path, instance)
+    print("Prédictions effectuées avec succès!")
+    print("Le prix de la maison est estimé à: ", predictions[0])
+    return predictions[0]
 
+
+# df = pd.read_csv('../home-data-for-ml-course/train.csv')
+
+# df = df.iloc[:1, :]
+# df = df[[
+#     'MSSubClass',
+#     'MSZoning',
+#     'LotFrontage',
+#     'LotArea',
+#     'Street',
+#     'LotShape',
+#     'LandContour',
+#     'Utilities',
+#     'LotConfig',
+#     'LandSlope',
+#     'Neighborhood',
+#     'Condition1',
+#     'Condition2',
+#     'BldgType',
+#     'HouseStyle',
+#     'OverallQual',
+#     'OverallCond',
+#     'YearBuilt',
+#     'YearRemodAdd',
+#     'RoofStyle',
+#     'RoofMatl',
+#     'Exterior1st',
+#     'Exterior2nd',
+#     'MasVnrType',
+#     'MasVnrArea',
+#     'ExterQual',
+#     'ExterCond',
+#     'Foundation',
+#     'BsmtQual',
+#     'BsmtCond',
+#     'BsmtExposure',
+#     'BsmtFinType1',
+#     'BsmtFinSF1',
+#     'BsmtFinSF2',
+#     'BsmtUnfSF',
+#     'TotalBsmtSF',
+#     'Heating',
+#     'HeatingQC',
+#     'CentralAir',
+#     'Electrical',
+#     '1stFlrSF',
+#     '2ndFlrSF',
+#     'LowQualFinSF',
+#     'GrLivArea',
+#     'BsmtFullBath',
+#     'BsmtHalfBath',
+#     'FullBath',
+#     'HalfBath',
+#     'BedroomAbvGr',
+#     'KitchenAbvGr',
+#     'KitchenQual',
+#     'TotRmsAbvGrd',
+#     'Functional',
+#     'Fireplaces',
+#     'FireplaceQu',
+#     'GarageType',
+#     'GarageFinish',
+#     'GarageCars',
+#     'GarageQual',
+#     'PavedDrive',
+#     'WoodDeckSF',
+#     'OpenPorchSF',
+#     'EnclosedPorch',
+#     '3SsnPorch',
+#     'ScreenPorch',
+#     'PoolArea',
+#     'MiscVal',
+#     'MoSold',
+#     'YrSold',
+#     'SaleType',
+#     'SaleCondition'
+# ]]
+
+# liste = df.values.tolist()[0]
+# print(liste)
+# print(type(liste))
 
 
 if __name__ == "__main__":
-    test()
+    # estimate_its_house(liste)
